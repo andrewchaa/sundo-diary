cbb04b5aa0a12b10281844743e16bac66d2a129e
Andrew Chaa <andrew.yh.chaa@gmail.com>
2018-07-05 20:52:03 +0100

    title

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,16 +3,8 @@ import Link from 'next/link'
 const Navbar = () => (
   <nav className="navbar navbar-expand navbar-dark bg-dark mb-4">
     <div className="container">
-      <a className="navbar-brand" href="#">BitzPrice</a>
+      <a className="navbar-brand" href="/">선도 체험기</a>
       <div className="collapse navbar-collapse">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link href="/"><a className="nav-link">Home</a></Link>
-          </li>
-          <li className="nav-item">
-            <Link href="/about"><a className="nav-link">About</a></Link>
-          </li>
-        </ul>
       </div>
     </div>
   </nav>
